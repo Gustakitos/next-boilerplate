41424301b20a984b9db8b5fc338bba1d12db258b
Luis Gustavo <luis.araujo@waveapps.com>
2024-11-05 10:52:36 -0300

    feat: add configs to storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react'
 import * as S from './styles'
 
-export default function Main() {
-  return <S.Wrapper>Main</S.Wrapper>
+export default function Main({
+  title = 'React Avançado',
+  description = 'Typescript, ReactJS, NextJS e Styled Components'
+}: {
+  title?: string
+  description?: string
+}) {
+  return (
+    <S.Wrapper>
+      <h1>React Avançado</h1>
+      <S.Logo
+        src="/img/logo.svg"
+        alt="Imagem de um átomo e React Avançado escrito ao lado."
+      />
+      <S.Title>{title}</S.Title>
+      <S.Description>{description}</S.Description>
+      <S.Illustration
+        src="/img/hero-illustration.svg"
+        alt="Um desenvolvedor de frente para uma tela com código."
+      />
+    </S.Wrapper>
+  )
 }
--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -4,9 +4,18 @@ import Main from '.'
 export default {
   title: 'Main',
   component: Main,
+  args: {
+    title: 'React Avançado',
+    description: 'Typescript, ReactJS, NextJS e Styled Components'
+  },
   parameters: {
     layout: 'fullscreen'
   }
 } as Meta
 
-export const Default: StoryObj = {}
+export const Default: StoryObj = {
+  args: {
+    title: 'title basic',
+    description: 'description basic'
+  }
+}
